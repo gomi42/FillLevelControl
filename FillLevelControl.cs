4fe8b73914062c2cb5f0c01d43c99543c44e9f22
gomi42 <gomi42@gmx.de>
2023-08-16 08:23:02 +0200

    Add project files.

---
+++ FillLevelControl.cs
@@ -0,0 +1,114 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace FillLevelControlTest
+{
+    public class FillLevelControl : Control
+    {
+        private FrameworkElement container;
+        private Rectangle fillLevelIndicator;
+
+        public FillLevelControl()
+        {
+            SizeChanged += OnSizeChanged;
+        }
+
+        public double FillLevel
+        {
+            get => (double)GetValue(FillLevelProperty);
+            set => SetValue(FillLevelProperty, value);
+        }
+
+        public static readonly DependencyProperty FillLevelProperty =
+            DependencyProperty.Register("FillLevel", typeof(double), typeof(FillLevelControl), new FrameworkPropertyMetadata(20.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, FillLevelChanged));
+
+        private static void FillLevelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((FillLevelControl)d).OnFillLevelChanged();
+        }
+
+        private void OnFillLevelChanged()
+        {
+            SetFillLevel();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            container = GetTemplateChild("Part_Container") as FrameworkElement;
+            fillLevelIndicator = GetTemplateChild("Part_FillLevel") as Rectangle;
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            SetFillLevel();
+        }
+
+        private void SetFillLevel()
+        {
+            if (fillLevelIndicator == null)
+            {
+                return;
+            }
+
+            double fillLevel = FillLevel / 100.0;
+
+            var height = container.ActualHeight * fillLevel;
+            fillLevelIndicator.Height = height;
+
+            var fillColor = GetFillColor(fillLevel);
+            fillLevelIndicator.Fill = new SolidColorBrush(fillColor);
+        }
+
+        private GradientStop[] gradientStops = new GradientStop[] 
+        {
+            new GradientStop(Color.FromRgb(255, 0, 0), 0),
+            new GradientStop(Color.FromRgb(255, 255, 0), 0.4),
+            new GradientStop(Color.FromRgb(0, 255, 255), 0.6),
+            new GradientStop(Color.FromRgb(0, 255, 0), 1)
+        };
+
+        private Color GetFillColor(double fillLevel)
+        {
+            int lastStop = gradientStops.Count() - 1;
+
+            if (lastStop < 0)
+            {
+                return Colors.Transparent;
+            }
+
+            if (fillLevel >= gradientStops[lastStop].Offset)
+            {
+                return gradientStops[lastStop].Color;
+            }
+
+            if (fillLevel <= gradientStops[0].Offset)
+            {
+                return gradientStops[0].Color;
+            }
+
+            int index = 0;
+
+            while (fillLevel > gradientStops[index + 1].Offset)
+            {
+                index++;
+            }
+
+            var fl = (fillLevel - gradientStops[index].Offset) / (gradientStops[index + 1].Offset - gradientStops[index].Offset);
+            var nfl = 1 - fl;
+
+            Color color1 = gradientStops[index + 1].Color;
+            Color color2 = gradientStops[index].Color;
+
+            byte r = (byte)(color1.R * fl + color2.R * nfl);
+            byte g = (byte)(color1.G * fl + color2.G * nfl);
+            byte b = (byte)(color1.B * fl + color2.B * nfl);
+
+            return Color.FromRgb(r, g, b);
+        }
+    }
+}
---
+++ ShapeClipConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FillLevelControlTest
+{
+    internal class ShapeClipConverter : IValueConverter
+    {
+        /// The aspect ratio (height/width) of the geometry
+        /// </summary>
+        public const double AspectRatio = 1.11333990237159;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            return CreateFromHeight((double)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Create the geometry from the given height and keep the original aspect ratio
+        /// Shapes extracted from file "D:\Programme\Tools\FillLevelControlTest\ComplexShape.svg"
+        /// Generated from the following stream geometry:
+        /// F0M182.85714,213.94826 L574.28572,116.8054 L679.99997,488.23397 L491.42857,373.94826 L437.14284,776.80539 L108.57143,719.66253 L385.71428,388.23398 z
+        /// </summary>
+        /// <param name="height">the height in WPF units</param>
+        /// <returns>Returns the geometry</returns>
+        private Geometry CreateFromHeight(double height)
+        {
+            double scale = height;
+            StreamGeometry geometry = new StreamGeometry();
+            geometry.FillRule = FillRule.EvenOdd;
+            StreamGeometryContext ctx = geometry.Open();
+
+            ctx.BeginFigure(new Point(0.11255 * scale, 0.14719 * scale), true, true);
+            ctx.LineTo(new Point(0.70563 * scale, 0), true, true);
+            ctx.LineTo(new Point(0.8658 * scale, 0.56277 * scale), true, true);
+            ctx.LineTo(new Point(0.58009 * scale, 0.38961 * scale), true, true);
+            ctx.LineTo(new Point(0.49784 * scale, scale), true, true);
+            ctx.LineTo(new Point(0, 0.91342 * scale), true, true);
+            ctx.LineTo(new Point(0.41991 * scale, 0.41126 * scale), true, true);
+
+            ctx.Close();
+            geometry.Freeze();
+
+            return geometry;
+        }
+    }
+}
